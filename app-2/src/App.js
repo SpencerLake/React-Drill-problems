24db01a097c08c4f432c495de05cb5c01ca2e46b
Spencer-Lake <spencer.m.lake11@gmail.com>
2021-10-22 18:11:48 -0400

    finished problem 3

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -2,22 +2,41 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// class App extends Component {
+//   constructor() {
+//     super()
+//     this.state = {
+//       data: ["spaghetti", "ice cream", "sushi", "bologna", "cheese"]
+//     }
+//   }
+
+//   render() {
+//     let foodsToDisplay = this.state.data.map((element, index) => {
+//       return <h2 key={index}>{element}</h2>
+//     })
+//     return(
+//       <div className="App">{foodsToDisplay}</div>
+//     )
+//   }
+// }
+
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      data: ["spaghetti", "ice cream", "sushi", "bologna", "cheese"]
+      foods: ["spaghetti", "ice cream", "sushi", "bologna", "cheese"]
     }
   }
 
   render() {
-    let foodsToDisplay = this.state.data.map((element, index) => {
+    let displayFoods = this.state.foods.map((element, index) => {
       return <h2 key={index}>{element}</h2>
     })
     return(
-      <div className="App">{foodsToDisplay}</div>
+      <div className="App">{displayFoods}</div>
     )
   }
+
 }
 
 export default App;
